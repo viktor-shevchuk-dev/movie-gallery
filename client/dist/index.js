68181e1c346971cd2a1cd3818e65f2f93c28abf4
Victor <victor.shevchuk.96@gmail.com>
2022-10-20 10:55:09 +0300

    test

--- client/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-var rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
-var root = ReactDOM.createRoot(rootElement);
-root.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
