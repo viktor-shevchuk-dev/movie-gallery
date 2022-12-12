d4203e357c90028ef124533ca8c976f8f940be8e
Victor <victor.shevchuk.96@gmail.com>
2022-12-12 09:51:02 +0200

    removed unnecessary state

--- client/src/components/Main/Main.component.jsx
+++ client/src/components/Main/Main.component.jsx
@@ -1,4 +1,3 @@
-import { useReducer, useEffect, useCallback } from "react";
 import { useParams, useLocation } from "react-router-dom";
 
 import { MoviesList, MovieShelfFilterBar, ErrorBoundary } from "components";
@@ -7,30 +6,11 @@ import classes from "./Main.module.css";
 
 import { useGetMovieShelfQuery } from "services";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "genreOption": {
-      return {
-        ...state,
-        genreOption: action.genreOption,
-      };
-    }
-    case "sortOption": {
-      return {
-        ...state,
-        sortOption: action.sortOption,
-      };
-    }
-    default: {
-      throw Error("Unknown action: " + action.type);
-    }
-  }
-};
-
 export const Main = () => {
-  const [{ genreOption, sortOption }, dispatch] = useReducer(reducer, {});
-
   const { searchQuery } = useParams();
+  const { search } = useLocation();
+  const sortOption = new URLSearchParams(search).get("sortBy");
+  const genreOption = new URLSearchParams(search).get("genre");
 
   const {
     data: movieShelf,
@@ -38,33 +18,6 @@ export const Main = () => {
     isError,
     error,
   } = useGetMovieShelfQuery({ genreOption, sortOption, searchQuery });
-
-  const location = useLocation();
-  const { search } = location;
-  const sortByURLSearchParam = new URLSearchParams(search).get("sortBy");
-  const genreURLSearchParam = new URLSearchParams(search).get("genre");
-
-  const setGenreOption = useCallback(
-    (genreOption) => dispatch({ type: "genreOption", genreOption }),
-    []
-  );
-
-  const setSortOption = useCallback(
-    (sortOption) => dispatch({ type: "sortOption", sortOption }),
-    []
-  );
-
-  useEffect(() => {
-    if (!sortByURLSearchParam) return;
-
-    setSortOption(sortByURLSearchParam);
-  }, [setSortOption, sortByURLSearchParam]);
-
-  useEffect(() => {
-    if (!genreURLSearchParam) return;
-
-    setGenreOption(genreURLSearchParam);
-  }, [setGenreOption, genreURLSearchParam]);
 
   return (
     <main className={classes.main}>
